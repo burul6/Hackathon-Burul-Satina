8dda8ce3e95ba9b972f57af75f55824f7ff57f5a
burul6 <burul.baktybekova05@gmail.com>
2021-12-11 15:25:16 +0600

    edit header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import { BrowserRouter, Routes } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import "./App.css";
 // import Routing from './Routing';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header /> 
-          <Routes>
-            {/* <Routing /> */}
-          </Routes>
-        <Footer />
+        <Routes>
+          <Header />
+          {/* <Routing /> */}
+          <Footer />
+        </Routes>
       </BrowserRouter>
     </div>
   );
